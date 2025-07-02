241de339c9d4fd54b9b1b259ffa0e19832b05f5a
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-02 14:29:20 +0900

    CalendarView、GraphView のプレビュー用スタブを追加

--- Zeny/UI/CalendarView.swift
+++ Zeny/UI/CalendarView.swift
@@ -5,13 +5,22 @@
 //  Created by 永田健人 on 2025/07/02.
 //
 
-// UI/TabItem.swift
 import SwiftUI
 
 struct CalendarView: View {
     var body: some View {
+        // スタブ：別ブランチで実装中
         Text("Calendar View")
             .font(.largeTitle)
             .foregroundColor(.orange)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.orange.opacity(0.1))
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+            .previewLayout(.device)
     }
 }
