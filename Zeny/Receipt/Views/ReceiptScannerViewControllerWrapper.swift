11ee47ee83a249b966b8b2161d0c566471b4cc3b
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-02 14:43:55 +0900

    WrapperをBinding対応に修正し、OCR結果をrecognizedTextにバインド

--- Zeny/Receipt/Views/ReceiptScannerViewControllerWrapper.swift
+++ Zeny/Receipt/Views/ReceiptScannerViewControllerWrapper.swift
@@ -1,11 +1,26 @@
-// ReceiptScannerViewControllerWrapper.swift
 import SwiftUI
 
 struct ReceiptScannerViewControllerWrapper: UIViewControllerRepresentable {
-    typealias UIViewControllerType = ReceiptScannerViewController
+    @Binding var recognizedText: String 
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIViewController(context: Context) -> ReceiptScannerViewController {
-        ReceiptScannerViewController()
+        let vc = ReceiptScannerViewController()
+        // VC 側のクロージャに SwiftUI の binding を渡す
+        vc.onRecognized = { text in
+            DispatchQueue.main.async {
+                context.coordinator.parent.recognizedText = text
+            }
+        }
+        return vc
     }
     func updateUIViewController(_ uiViewController: ReceiptScannerViewController, context: Context) {}
+
+    class Coordinator: NSObject {
+        var parent: ReceiptScannerViewControllerWrapper
+        init(_ parent: ReceiptScannerViewControllerWrapper) { self.parent = parent }
+    }
 }
