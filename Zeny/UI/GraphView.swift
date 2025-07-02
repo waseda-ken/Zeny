1d97ff572cad0aeed772c53b3d5fb8e45b32216c
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-02 14:31:31 +0900

    CalendarView、GraphView のプレビュー用スタブを追加

--- Zeny/UI/GraphView.swift
+++ Zeny/UI/GraphView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct GraphView: View {
     var body: some View {
+        // スタブ：別ブランチで実装中
         Text("Graph View")
             .font(.largeTitle)
             .foregroundColor(.green)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.green.opacity(0.1))
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+            .previewLayout(.device)
     }
 }
