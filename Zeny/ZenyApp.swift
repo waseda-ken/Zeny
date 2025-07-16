985928e48c91191c965333d8d7cf5b9801cf638e
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-16 14:26:55 +0900

    ZenyApp: ナビバーにBlurテーマとゴールドアクセントカラーを適用

--- Zeny/ZenyApp.swift
+++ Zeny/ZenyApp.swift
@@ -1,11 +1,20 @@
-// ZenyApp.swift
 import SwiftUI
 
 @main
 struct ZenyApp: App {
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance  = appearance
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .accentColor(Color("AccentGold"))
+                .environment(\.colorScheme, .light)
         }
     }
 }
