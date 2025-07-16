b86927a949487aedb7536aca0c407b714d490b7c
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-16 14:06:14 +0900

    ImagePickerを実装しフォトライブラリ選択機能を追加

--- Zeny/UI/ImagePicker.swift
+++ Zeny/UI/ImagePicker.swift
@@ -1,18 +1,38 @@
-//
-//  ImagePicker.swift
-//  Zeny
-//
-//  Created by 永田健人 on 2025/07/16.
-//
-
+// UI/ImagePicker.swift
 import SwiftUI
+import UIKit
 
-struct ImagePicker: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    let sourceType: UIImagePickerController.SourceType
+
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
     }
-}
 
-#Preview {
-    ImagePicker()
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { Coordinator(self) }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        init(_ parent: ImagePicker) { self.parent = parent }
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
 }
