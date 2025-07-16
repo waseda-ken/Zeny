609597f9f26af29b100deaf9e9a99f3f00581685
waseda-ken <nagaken_0223@i.softbank.jp>
2025-07-16 14:05:32 +0900

    OCRServiceにrecognize(image:completion:)メソッドを追加し、sharedインスタンスを定義

---
+++ Zeny/UI/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  Zeny
+//
+//  Created by 永田健人 on 2025/07/16.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
